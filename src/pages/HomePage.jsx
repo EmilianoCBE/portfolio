498e7e4ef23ad8b48356b78420a05d742175e867
EmilianoCBE <e.carrizales1305@gmail.com>
2024-05-01 17:49:20 -0600

    Creación de HP e instalación de ant design

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+export const HomePage = () => {
+  return (
+    <>
+      <div>
+        <h1>Portafolio Personal</h1>
+        <h4>Hola, ¿cómo te va?</h4>
+        <p>Me llamo <strong>Emiliano</strong> y soy un <strong>Frontend Web Developer</strong></p>
+      </div>
+    </>
+  )
+}
