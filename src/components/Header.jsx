d4cd96db8a90996722b7bd2e83cf79e242e2078b
EmilianoCBE <e.carrizales1305@gmail.com>
2024-05-01 21:09:00 -0600

    Cambios en creación de sections header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,34 @@
+import { GithubFilled, LinkedinFilled } from '@ant-design/icons'
+import './Header.css'
+
 export const Header = () => {
+
   return (
-    <div>
-      
-    </div>
+    <>
+    <header className='header'>
+      <div>
+        <h3>
+          <a href="#hero">Portfolio</a>
+        </h3>
+      </div>
+      <div className='header-links'>
+        <ul>
+          <li>
+            <a href="#projects">Proyectos</a>
+          </li>
+          <li>
+            <a href="#about">Sobre mí</a>
+          </li>
+          <li>
+            <a href="#contact">Contacto</a>
+          </li>
+        </ul>
+      </div>
+      <div className='header-icons'>
+        <GithubFilled />
+        <LinkedinFilled />
+      </div>
+    </header>
+    </>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,49 @@
+import { useRef } from 'react'
+import './HomePage.css'
+import { Header } from "../components/Header"
+
 export const HomePage = () => {
+
+  const heroRef = useRef(null)
+  const projectsRef = useRef(null)
+  const aboutRef = useRef(null)
+  const contactRef = useRef(null)
+
+  const scrollRef = (ref) => {
+    window.scrollTo({
+      top: ref.current.offsetTop,
+      behavior: 'smooth'
+    })
+  }
+
   return (
-    <>
-      <div>
-        <h1>Portafolio Personal</h1>
-        <h4>Hola, ¿cómo te va?</h4>
-        <p>Me llamo <strong>Emiliano</strong> y soy un <strong>Frontend Web Developer</strong></p>
-      </div>
-    </>
+    <div>
+      <Header />
+      <section ref={heroRef} className='homepage-section' id='hero'>
+        <div>
+          <h1>Portafolio Personal</h1>
+          <h4>Hola, ¿cómo te va?</h4>
+          <p>Me llamo <strong>Emiliano</strong> y soy un <strong>Frontend Web Developer</strong></p>
+        </div>
+      </section>
+      <section ref={projectsRef} className='homepage-section' id='projects'>
+        <div>
+          <h1>Proyectos</h1>
+          
+        </div>
+      </section>
+      <section ref={aboutRef} className='homepage-section' id='about'>
+        <div>
+          <h1>Sobre mí</h1>
+          
+        </div>
+      </section>
+      <section ref={contactRef} className='homepage-section' id='contact'>
+        <div>
+          <h1>Contacto</h1>
+          
+        </div>
+      </section>
+    </div>
   )
 }
