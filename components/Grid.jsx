6c7113be3c95419d2f46204f6f29b3061e4c3c23
EmilianoCBE <e.carrizales1305@gmail.com>
2024-07-22 20:22:54 -0600

    Grid added

--- components/Grid.jsx
+++ components/Grid.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/bento-grid'
+import { gridItems } from '@/data/index'
 
 export const Grid = () => {
   return (
-    <div>Grid</div>
+    <section
+      id='about'
+    > 
+      <BentoGrid>
+        {
+          gridItems.map((project, index) => (
+            <BentoGridItem
+              key={index}
+              id={project.id}
+              title={project.title}
+              description={project.des}
+            />
+          ))
+        }
+      </BentoGrid>
+    </section>
   )
 }
