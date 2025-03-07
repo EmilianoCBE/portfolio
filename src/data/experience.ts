cdda8b3696de93213980d2442b9cef66da66a15b
EmilianoCBE <e.carrizales1305@gmail.com>
2025-03-07 07:58:33 -0600

    Correcting typos

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -6,7 +6,7 @@ export const experience: Experience[] = [
     company: "National Unity Insurance Company",
     location: "San Pedro Garza García, NL",
     date: "Ago 2023 - Present",
-    description: "Desarrolladro Frontend en la empresa de seguros National Unity, trabajando en el desarrollo de la página web de la empresa, así como en el diseño y creación de una nueva aplicación web para todas las áreas de la empresa."
+    description: "Desarrollador Frontend en la empresa de seguros National Unity, trabajando en el desarrollo de la página web de la empresa, así como en el diseño y creación de una nueva aplicación web para todas las áreas de la empresa."
   },
   {
     title: "Frontend Developer",
